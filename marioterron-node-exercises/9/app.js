13b3aaaeb42ed751fa611ca5c325a254a93d1a09
Clark Oake <clarkoake13@gmail.com>
2020-12-04 15:30:18 -0330

    complete exercise 9

---
+++ marioterron-node-exercises/9/app.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+const searchTerm = 'Jennifer';
+
+const readList = (searchTerm) => {
+    fs.readFile('comparison.txt', 'utf8', (err, data)=>{
+        if(err){
+            console.log(err);
+        }
+        let database = [];
+        let matchCounter = 0;
+        database = data.split('\r\n');
+        console.log(database);
+        database.forEach(term => {
+            if(term === searchTerm) {
+                matchCounter++;
+            }
+        })
+        console.log(`we found ${matchCounter} matches to your search term in our database`);
+    })
+}
+
+readList(searchTerm);
